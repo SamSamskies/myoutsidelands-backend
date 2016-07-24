4bb588e42a96c7b7b3a010050966639dc23303de
Steven Miyakawa <steven.miyakawa@here.com>
2016-07-23 19:29:42 -0700

    Combine top artists and followed artists

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,3 +1,5 @@
+const Promise = require('bluebird');
+const _ = require('underscore');
 const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const router = express.Router();
@@ -5,6 +7,7 @@ const router = express.Router();
 const scopes = [
   'user-top-read',
   'user-read-email',
+  'user-follow-read'
 ];
 
 router.get('/', function(req, res, next) {
@@ -26,14 +29,25 @@ router.get('/auth-callback', function(req, res, next) {
 
   spotifyApi.authorizationCodeGrant(req.query.code)
     .then((data) => spotifyApi.setAccessToken(data.body.access_token))
-    .then(() => spotifyApi.getMyTopArtists({ limit: 50 }))
-    .then((data) => {
-      const artists = data.body.items.map(({ name }) => name);
-      res.json(artists)
-    })
+    .then(() => getLikedArtists(spotifyApi))
+    .then((likedArtists) => ({ likedArtists }))
+    .then((payload) => res.json(payload))
     .catch((err) => req.json(err))
 });
 
+function getTopArtists(spotifyApi) {
+  return spotifyApi.getMyTopArtists({ limit: 50 })
+    .then((data) => data.body.items.map(({ name }) => name.toLowerCase()));
+}
 
+function getFollowedArtists(spotifyApi) {
+  return spotifyApi.getFollowedArtists({ limit: 50 })
+    .then((data) => data.body.artists.items.map(({ name }) => name.toLowerCase()));
+}
+
+function getLikedArtists(spotifyApi) {
+  return Promise.all([getTopArtists(spotifyApi), getFollowedArtists(spotifyApi)])
+    .spread((topArtists, followedArtists) => _.uniq(topArtists.concat(followedArtists)));
+}
 
 module.exports = router;
