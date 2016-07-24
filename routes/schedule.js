44d6c29311e34e8088d5b8afb68b8fad100d2bac
Steven Miyakawa <steven.miyakawa@here.com>
2016-07-23 20:28:53 -0700

    Get recommended artists

--- routes/schedule.js
+++ routes/schedule.js
@@ -30,24 +30,54 @@ router.get('/auth-callback', function(req, res, next) {
   spotifyApi.authorizationCodeGrant(req.query.code)
     .then((data) => spotifyApi.setAccessToken(data.body.access_token))
     .then(() => getLikedArtists(spotifyApi))
-    .then((likedArtists) => ({ likedArtists }))
+    .then(({ names, topTenArtistIds }) => {
+      return Promise.props({
+        likedArtists: names,
+        recommendedArtists: getRecommendedArtists(spotifyApi, topTenArtistIds)
+      });
+    })
+    .then(({ likedArtists, recommendedArtists }) => {
+      return {
+        likedArtists,
+        recommendedArtists: _.reject(recommendedArtists, (artist) => likedArtists.includes(artist))
+      }
+    })
     .then((payload) => res.json(payload))
-    .catch((err) => req.json(err))
+    .catch((err) => res.json(err))
 });
 
 function getTopArtists(spotifyApi) {
   return spotifyApi.getMyTopArtists({ limit: 50 })
-    .then((data) => data.body.items.map(({ name }) => name.toLowerCase()));
+    .then((data) => data.body.items);
 }
 
 function getFollowedArtists(spotifyApi) {
   return spotifyApi.getFollowedArtists({ limit: 50 })
-    .then((data) => data.body.artists.items.map(({ name }) => name.toLowerCase()));
+    .then((data) => data.body.artists.items);
 }
 
 function getLikedArtists(spotifyApi) {
   return Promise.all([getTopArtists(spotifyApi), getFollowedArtists(spotifyApi)])
-    .spread((topArtists, followedArtists) => _.uniq(topArtists.concat(followedArtists)));
+    .spread((topArtists, followedArtists) => {
+      const topArtistNames = _.pluck(topArtists, 'name').map((name) => name.toLowerCase());
+      const followedArtistNames = _.pluck(followedArtists, 'name').map((name) => name.toLowerCase());
+      const topTenArtistIds = _.pluck(_.first(topArtists, 10), 'id');
+
+      return {
+        names: _.uniq(topArtistNames.concat(followedArtistNames)),
+        topTenArtistIds
+      };
+    });
+}
+
+function getRecommendedArtists(spotifyApi, topTenArtistIds) {
+  return Promise.map(topTenArtistIds, (id) => getRelatedArtists(spotifyApi, id))
+    .then((arrayOfRelatedArtists) => _.uniq(_.flatten(arrayOfRelatedArtists)));
+}
+
+function getRelatedArtists(spotifyApi, id) {
+  return spotifyApi.getArtistRelatedArtists(id)
+    .then((data) => _.pluck(data.body.artists, 'name').map((name) => name.toLowerCase()));
 }
 
 module.exports = router;
