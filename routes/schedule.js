ea82a43aff4ad1e8455a7878ae67f079634ed09f
Steven Miyakawa <steven.miyakawa@here.com>
2016-07-23 18:20:05 -0700

    Add auth and return top 50 artists

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,8 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const router = express.Router();
+
+const scopes = [
+  'user-top-read',
+  'user-read-email',
+];
 
 router.get('/', function(req, res, next) {
-  res.json({ test: 'hello worldddd'});
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    redirectUri: process.env.SPOTIFY_AUTH_CALLBACK
+  });
+  const authorizeUrl = spotifyApi.createAuthorizeURL(scopes)
+
+  res.redirect(authorizeUrl);
 });
+
+router.get('/auth-callback', function(req, res, next) {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_AUTH_CALLBACK
+  });
+
+  spotifyApi.authorizationCodeGrant(req.query.code)
+    .then((data) => spotifyApi.setAccessToken(data.body.access_token))
+    .then(() => spotifyApi.getMyTopArtists({ limit: 50 }))
+    .then((data) => {
+      const artists = data.body.items.map(({ name }) => name);
+      res.json(artists)
+    })
+    .catch((err) => req.json(err))
+});
+
+
 
 module.exports = router;
